6ab7b9d12e50a153896436173511d6df19a06bd7
Jesús Germade <jesus@germade.es>
2018-09-12 09:44:59 +0200

    tweaks

--- directives/on.js
+++ directives/on.js
@@ -2,7 +2,7 @@
 module.exports = function (APP, TEXT, directive_ns) {
   APP.directive(directive_ns + '-on:\\w+', function (node_el, node, render_options, _with_node) {
     var event_name = this.attr_key.substr(directive_ns.length + 4),
-        onTrigger = new Function('data', 'with(data) { return (' + this.attr_value + '); };'),
+        onTrigger = new Function('data', 'with(data) { ' + this.attr_value + ' };'),
         data = render_options && render_options.data || {}; // '-on:'.length === 4
 
     node_el.addEventListener(event_name, function () {
