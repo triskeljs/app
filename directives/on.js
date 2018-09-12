34afc5733aaff55699e148660b39ada11e0c932e
Jesús Germade <jesus@germade.es>
2018-09-12 14:05:04 +0200

    updateData after trigger on event then,catch if is a promise

--- directives/on.js
+++ directives/on.js
@@ -3,7 +3,7 @@ module.exports = function (APP, TEXT, directive_ns) {
   APP.directive(directive_ns + '-on:\\w+', function (node_el, node, render_options, _with_node) {
     var _this = this,
         event_name = this.attr_key.substr(directive_ns.length + 4),
-        onTrigger = new Function('data', 'with(data) { ' + this.attr_value + ' };'),
+        onTrigger = new Function('data', 'with(data) { return(' + this.attr_value + ') };'),
         data = render_options && render_options.data || {}; // '-on:'.length === 4
 
     node_el.addEventListener(event_name, function () {
