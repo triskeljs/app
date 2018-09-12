780667c0ca72de20f8588ad2ba0518b952e2d66c
Jesús Germade <jesus@germade.es>
2018-09-12 11:54:55 +0200

    updateData after trigger on event

--- directives/on.js
+++ directives/on.js
@@ -1,15 +1,17 @@
 
 module.exports = function (APP, TEXT, directive_ns) {
   APP.directive(directive_ns + '-on:\\w+', function (node_el, node, render_options, _with_node) {
-    var event_name = this.attr_key.substr(directive_ns.length + 4),
+    var _this = this,
+        event_name = this.attr_key.substr(directive_ns.length + 4),
         onTrigger = new Function('data', 'with(data) { ' + this.attr_value + ' };'),
         data = render_options && render_options.data || {}; // '-on:'.length === 4
 
     node_el.addEventListener(event_name, function () {
       onTrigger.call(node_el, data);
+      _this.updateData();
     });
 
-    this.watchData(function (_data) {
+    _this.watchData(function (_data) {
       data = _data;
     });
   });
