4bc0ea42ee40b1c4ca9b4a7c9ec081d68031dbc2
Jesús Germade <jesus@germade.es>
2018-12-06 04:29:30 +0100

    @triskel/render@1.1.7

--- tests/_karma-app.js
+++ tests/_karma-app.js
@@ -171,36 +171,24 @@
   }
 
   function _appendChildren (parent_el, nodes, ns_scheme, options, _withNode, inits_list) {
-    var inserted_nodes = [],
-        insert_before = options.insert_before;
-
-    options.insert_before = null;
-
-    var _dom_fragment = nodes.length && document.createDocumentFragment();
+    var inserted_nodes = [];
 
     for( var i = 0, n = nodes.length, node ; i < n ; i++ ) {
       node = nodes[i];
-    // nodes.forEach(function (node) {
 
       if( typeof node === 'string' ) node = { text: node };
-      // if( options.remove_comments && node && typeof node === 'object' && 'comments' in node ) return
       if( options.remove_comments && node && typeof node === 'object' && 'comments' in node ) break
 
       var with_node = _withNode(node) || {};
       var node_el;
 
       if( with_node.replace_by_comment ) node_el = document.createComment(with_node.replace_by_comment);
-      else node_el = _create(node, parent_el, ns_scheme, options, _withNode, inits_list, with_node.replace_text);
+      else node_el = _create(node, ns_scheme, options, _withNode, inits_list, with_node.replace_text);
 
       if( with_node.onCreate instanceof Function ) with_node.onCreate.call(node_el, node_el, node, options, with_node);
 
-      if( _dom_fragment ) _dom_fragment.appendChild(node_el);
-      else if( insert_before ) parent_el.insertBefore(node_el, insert_before);
-      else parent_el.appendChild( node_el );
+      parent_el.appendChild( node_el );
 
-      // if( with_node.initNode ) inits_list.push(function () {
-      //   with_node.initNode.call(node_el, node_el, node, options, with_node)
-      // })
       if( with_node.initNode ) inits_list.push({
         fn: with_node.initNode,
         _this: node_el,
@@ -214,11 +202,6 @@
       });
 
     }
-    // })
-    if( !_dom_fragment ) return inserted_nodes
-
-    if( insert_before ) parent_el.insertBefore(_dom_fragment, insert_before);
-    else parent_el.appendChild(_dom_fragment);
 
     return inserted_nodes
   }
@@ -229,7 +212,7 @@
     xul: 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul',
   };
 
-  function _create(node, _parent, ns_scheme, options, _withNode, inits_list, replace_text) {
+  function _create(node, ns_scheme, options, _withNode, inits_list, replace_text) {
     var node_el;
 
     if( 'text' in node ) return document.createTextNode( replace_text === undefined ? node.text : replace_text )
@@ -252,21 +235,28 @@
 
   function _runInits (init_node) { init_node.fn.apply(init_node._this, init_node._args); }
 
-  function renderNodes (parent, nodes, options) {
+  function renderNodes (parent_el, nodes, options) {
     options = Object.create(options || {});
-    var _withNode = options.withNode || function () {};
+    var _withNode = options.withNode || function __withNode () {};
+    var _insert_before = options.insert_before;
 
     options.withNode = null;
+    options.insert_before = null;
 
-    if( typeof options.withNode !== 'function' ) options.withNode = function () {};
-
-    if( !options.insert_before && options.keep_content !== true ) {
-      while( parent.firstChild )
-        parent.removeChild(parent.firstChild);
+    if( !_insert_before && options.keep_content !== true ) {
+      while( parent_el.firstChild )
+      parent_el.removeChild(parent_el.firstChild);
     }
 
+    if( !nodes.length ) return []
+
+    var _dom_fragment = document.createDocumentFragment();
+
     var inits_list = [],
-        inserted_nodes = _appendChildren(parent, nodes, null, options, _withNode, inits_list);
+        inserted_nodes = _appendChildren(_dom_fragment, nodes, null, options, _withNode, inits_list);
+
+    if( _insert_before ) parent_el.insertBefore(_dom_fragment, _insert_before);
+    else parent_el.appendChild(_dom_fragment);
 
     inits_list.forEach(_runInits);
 
