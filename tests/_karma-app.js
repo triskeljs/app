eb352f611172423b5b10cfcb85094701764492b6
Jesús Germade <jesus@germade.es>
2018-12-06 01:42:00 +0100

    @triskel/render@1.1.3

--- tests/_karma-app.js
+++ tests/_karma-app.js
@@ -176,7 +176,7 @@
 
     options.insert_before = null;
 
-    var _dom_fragment = document.createDocumentFragment();
+    var _dom_fragment = nodes.length && document.createDocumentFragment();
 
     for( var i = 0, n = nodes.length, node ; i < n ; i++ ) {
       node = nodes[i];
@@ -194,9 +194,9 @@
 
       if( with_node.onCreate instanceof Function ) with_node.onCreate.call(node_el, node_el, node, options, with_node);
 
-      // if( insert_before ) parent_el.insertBefore(node_el, insert_before)
-      // else parent_el.appendChild( node_el )
-      _dom_fragment.appendChild(node_el);
+      if( _dom_fragment ) _dom_fragment.appendChild(node_el);
+      else if( insert_before ) parent_el.insertBefore(node_el, insert_before);
+      else parent_el.appendChild( node_el );
 
       // if( with_node.initNode ) inits_list.push(function () {
       //   with_node.initNode.call(node_el, node_el, node, options, with_node)
@@ -215,6 +215,7 @@
 
     }
     // })
+    if( !_dom_fragment ) return inserted_nodes
 
     if( insert_before ) parent_el.insertBefore(_dom_fragment, insert_before);
     else parent_el.appendChild(_dom_fragment);
