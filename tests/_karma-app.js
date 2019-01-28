081fbbb9af5809a655ab419c8056f5f69a148d03
Jesús Germade <jesus@germade.es>
2019-01-28 13:10:11 +0100

    data-bind inherits data push

--- tests/_karma-app.js
+++ tests/_karma-app.js
@@ -734,12 +734,12 @@
 
       var parsed = TEXT.parseExpression(node.attrs[ directive_ns + '-bind']);
 
-      this.watchData(function () {
+      this.watchData(function (_data) {
         var result = parsed.processFilters( TEXT.eval(parsed.expression, render_options.data) );
 
         if( typeof result !== 'string' && !(result instanceof Array) ) throw new TypeError('data-bind should return a String or an Array')
 
-        APP.render(node_el, typeof result === 'string' ? [{ text: result }] : result);
+        APP.render(node_el, typeof result === 'string' ? [{ text: result }] : result, { data: _data });
       });
 
     });
