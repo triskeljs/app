0121106417f210c3e286a68d11534c70be91d103
Jesús Germade <jesus@germade.es>
2020-05-19 02:29:32 +0200

    updating tests

--- tests/_karma-app.js
+++ tests/_karma-app.js
@@ -735,13 +735,21 @@
     APP.directive(directive_ns + '-bind', function (node_el, node, render_options, _with_node) {
 
       var parsed = TEXT.parseExpression(node.attrs[ directive_ns + '-bind']);
+      var current_content = null;
+      var current_view = null;
 
       this.watchData(function (_data) {
-        var result = parsed.processFilters( TEXT.eval(parsed.expression, render_options.data) );
+        var content = parsed.processFilters( TEXT.eval(parsed.expression, render_options.data) );
 
-        if( typeof result !== 'string' && !(result instanceof Array) ) throw new TypeError('data-bind should return a String or an Array')
+        if( typeof content !== 'string' && !(content instanceof Array) ) throw new TypeError('data-bind should return a String or an Array')
 
-        APP.render(node_el, typeof result === 'string' ? [{ text: result }] : result, { data: _data });
+        if (current_content && content === current_content) {
+          current_view.updateData(_data);
+        } else {
+          current_content = content;
+
+          current_view = APP.render(node_el, typeof content === 'string' ? [{ text: content }] : content, { data: _data });
+        }
       });
 
     });
